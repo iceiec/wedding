9092ac96bfda503a3011dc211a96aba631fd5e05
iceiec <isaiah.aguinaldo2@gmail.com>
2025-12-17 16:01:51 +0800

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";  // <-- correct path
 import "./styles/index.css";       // your Tailwind styles
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
